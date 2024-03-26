c062f95a815197c6adfa885e8cf0bbcc43c61b74
Kevin Azevedo <taylorvision95@outlook.com>
2024-03-26 16:25:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1, "Books");
+		Department department = new Department(1, "Atlantic Records");
+		Seller seller = new Seller(1, "Wiz", "wiz@gmail.com", new Date(), 7400.00, department);
 		
-		System.out.println(department);		
+		System.out.println(seller);	
 	}
 }
